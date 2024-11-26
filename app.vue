81b1b9ac6992fa07625d01e38618cdae2bb7e65a
TI-CBH <blake@totalinitiatives.com>
2024-11-25 18:33:06 -0600

    Properly handle initial routing Enforce authentication Redirect unauthenticated users to login Redirect authenticated users to the dashboard

--- app.vue
+++ app.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-      <NuxtPage />
-    </div>
-  </template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+// Force authentication on all routes except login
+const auth = useAuth()
+
+onMounted(() => {
+  // Initialize auth
+  if (process.client) {
+    auth.init()
+  }
+})
+
+// Redirect to login if not authenticated
+watchEffect(() => {
+  if (process.client && !auth.isLoading && !auth.isAuthenticated) {
+    navigateTo('/login')
+  }
+})
+</script>
